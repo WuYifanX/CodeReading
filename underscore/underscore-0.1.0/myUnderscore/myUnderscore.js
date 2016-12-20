ede20e0ce24f48a26b2d369a10519bdbc18d56cd
WuYifanX <510182912@qq.com>
2016-12-20 23:33:35 +0800

    Init this project

---
+++ underscore/underscore-0.1.0/myUnderscore/myUnderscore.js
@@ -0,0 +1,12 @@
+/**
+ * Created by wuyifan on 16/12/20.
+ */
+
+// This is my underscore for practice.
+
+window.__ = {
+    VERSION: "0.1.0",
+    each:function(obj,iterator,context){
+        
+    }
+};
